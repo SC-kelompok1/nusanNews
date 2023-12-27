d9465367677fbe58aaf667c1e0f96fb3e4df99d2
guns04 <agungpangestu960@gmail.com>
2023-12-27 22:20:55 +0700

    Update Post and Delete

--- controler/berita.controler.js
+++ controler/berita.controler.js
@@ -15,7 +15,39 @@ module.exports = {
   },
   getBeritaByJudul: (req, res) => {},
   getBeritaByKategori: (req, res) => {},
-  addBerita: (req, res) => {},
-  deleteBeritaByID: (req, res) => {},
+  addBerita: async (req, res) => {
+      try {
+        
+        const id = req.params.id
+        const {judul, artikel, kategori, foto} = req.body
+      
+        const data = await Berita.create({judul, artikel, kategori, foto
+        });
+        res.status(200).json({
+          message: "success",
+        })
+      } catch (error) {
+        res.status(500).json({
+          message:error.message})
+    }
+  },
+
+  deleteBeritaByID: async (req, res) => {
+    try {
+        const id = req.params.id
+        // Use the MongoDB model to find and delete the document by ID
+        const deletedBerita = await Berita.destroy({
+          where: {
+            id : id
+          }
+        });
+  
+        res.status(200).json({ message: 'Berita deleted successfully'  });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+  
+    }
+  },
   updateBerita: (req, res) => {},
 };
