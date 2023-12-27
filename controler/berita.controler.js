c9dd1e35d680006d7ed4be08f95a609e96fa5cb2
Allia14 <allialilac14@gmail.com>
2023-12-27 18:39:59 +0700

    Allia's part

--- controler/berita.controler.js
+++ controler/berita.controler.js
@@ -13,9 +13,52 @@ module.exports = {
       res.status(500).json({ message: "Maaf terjadi error", err: err.message });
     }
   },
-  getBeritaByJudul: (req, res) => {},
+  
+
+  getBeritaByJudul: async (req, res) => {
+    try {
+      const { judul } = req.params;
+      const berita = await Berita.findAll({
+        where: { judul },
+      });
+
+      if (!berita) {
+        return res.status(404).json({ message: "tidak ada datanya" });
+      }
+      res.status(200).json({
+        message: "success get data",
+        data: berita,
+      });
+    } catch (err) {
+      res.status(500).json({ message: "maaf error", err: err.message });
+    }
+  },
+
   getBeritaByKategori: (req, res) => {},
   addBerita: (req, res) => {},
   deleteBeritaByID: (req, res) => {},
-  updateBerita: (req, res) => {},
-};
+  updateBerita: async (req, res) => {
+
+    try {
+      const id = req.params.id
+      const {judul, artikel, kategori, foto} = req.body
+      const berita = await Berita.findOne({
+        where: {
+          id : id,
+        }
+      })
+      const data = await Berita.update({judul, artikel, kategori, foto}, {
+        where: {
+          id: berita.id,
+        }
+      });
+      res.status(200).json({
+        message: "success",
+      })
+    } catch (error) {
+      res.status(500).json({
+        message:error.message
+      })
+    }
+    }
+  }
