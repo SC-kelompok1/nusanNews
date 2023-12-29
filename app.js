8cd85b830f02b5af91fe483df5af77612df57d00
Javanese77 <aptaanugerah@gmail.com>
2023-12-29 22:30:18 +0700

    commit final

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const allRoutes = require("./routes");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(allRoutes);
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
